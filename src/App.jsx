9372378f76bd7411f41f2b4235988e08ed1ae94a
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2024-06-27 22:20:14 +0530

    updated my company logo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from './Componants/Navbar'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
-      
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+    <Navbar/>
     </div>
   )
 }
 
 export default App
+  
